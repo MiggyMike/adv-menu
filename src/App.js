0770e432589e396492bc6b3ecd59d62bd650ee4b
MICHAEL <MJENNINGS6@gmail.com>
2021-02-25 12:37:59 -0500

    set up first dropdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faComment, faPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+    faBell,
+    faCaretDown,
+    faChevronCircleRight,
+    faCog,
+    faComment,
+    faPlus,
+} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faPlus, faBell, faComment);
+library.add(
+    faPlus,
+    faBell,
+    faComment,
+    faCaretDown,
+    faCog,
+    faChevronCircleRight
+);
 
 function App() {
     return (
@@ -12,6 +26,10 @@ function App() {
                 <NavItem icon={<FontAwesomeIcon icon='plus' />} />
                 <NavItem icon={<FontAwesomeIcon icon='bell' />} />
                 <NavItem icon={<FontAwesomeIcon icon='comment' />} />
+
+                <NavItem icon={<FontAwesomeIcon icon='caret-down' />}>
+                    <DropdownMenu />
+                </NavItem>
             </Navbar>
         </div>
     );
@@ -26,12 +44,39 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+    const [open, setOpen] = useState(false);
     return (
         <li className='nav-item'>
-            <a href='#' className='icon-button'>
+            <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
                 {props.icon}
             </a>
+            {open && props.children}
         </li>
+    );
+}
+
+function DropdownMenu() {
+    function DropdownItem(props) {
+        return (
+            <a href='#' className='menu-item'>
+                <span className='icon-button'>{props.leftIcon}</span>
+                {props.children}
+                <span className='icon-right'>{props.rightIcon}</span>
+            </a>
+        );
+    }
+
+    return (
+        <>
+            <div className='dropdown'>
+                <DropdownItem>My Profile</DropdownItem>
+                <DropdownItem
+                    leftIcon={<FontAwesomeIcon icon='cog' />}
+                    rightIcon={
+                        <FontAwesomeIcon icon='Chevron-Circle-Right' />
+                    }></DropdownItem>
+            </div>
+        </>
     );
 }
 
