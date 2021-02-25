bca085f4f94c851ce95594118c416f9516e3dde2
MICHAEL <MJENNINGS6@gmail.com>
2021-02-25 11:58:59 -0500

    set functional components, and imported FA icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faComment, faPlus } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlus, faBell, faComment);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar>
+                <NavItem icon={<FontAwesomeIcon icon='plus' />} />
+                <NavItem icon={<FontAwesomeIcon icon='bell' />} />
+                <NavItem icon={<FontAwesomeIcon icon='comment' />} />
+            </Navbar>
+        </div>
+    );
+}
+
+function Navbar(props) {
+    return (
+        <nav className='navbar'>
+            <ul className='navbar-nav'>{props.children}</ul>
+        </nav>
+    );
+}
+
+function NavItem(props) {
+    return (
+        <li className='nav-item'>
+            <a href='#' className='icon-button'>
+                {props.icon}
+            </a>
+        </li>
+    );
 }
 
 export default App;
